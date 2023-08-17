18959d3a3f292e236bb73075abe852b25541d0a7
leeseungjun <cjstkrak@gmail.com>
2023-08-18 01:19:05 +0900

    nickname, message 구분해서 받아 처리

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,45 @@
-1112;
-sdsd;
+const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("conneted to server");
+});
+// 여기서의 소켓은 백엔드와의 연결
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+  // const translatedMessageData = message.toString();
+  // console.log(message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("연결이 끊겼어요");
+});
+
+// setTimeout(() => {
+//   socket.send("hello my browser");
+// }, 3000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  // const message = JSON.stringify(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  // nickForm.style.display = "none";
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,37 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/public/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("http://localhost:3000을 열었습니다.");
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+// http 와 ws를 동시에 실행
+const sockets = [];
+// 여기서의 소켓은 브라우저와의 연결
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log(`conneted to Browser 현재 ${sockets.length}`);
+  socket.on("close", () => console.log("연결끝"));
+  socket.on("message", (message) => {
+    // const translatedMessageData = JSON.parse(message);
+    message = message.toString("utf-8");
+    const msgObj = JSON.parse(message);
+    if (msgObj.type === "new_message") {
+      sockets.forEach((soc) => soc.send(msgObj.payload));
+    }
+  });
 });
-app.listen(3000, console.log("3000포트로 서버가 열렸습니다."));
+
+server.listen(3000, handleListen);
+
+// {
+//   type:"message",
+//   payload:"hello 모두들"
+// }
+// {
+//   type:"nickname",
+//   payload:"neo"
+// }
