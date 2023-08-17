a32fb6c4b60e826f7b15cddcd524312bfa303797
leeseungjun <cjstkrak@gmail.com>
2023-08-18 02:35:02 +0900

    install socketIO

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -13,30 +13,30 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("http://localhost:3000을 열었습니다.");
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIO(server);
 // http 와 ws를 동시에 실행
-const sockets = [];
-// 여기서의 소켓은 브라우저와의 연결
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log(`conneted to Browser 현재 ${sockets.length}`);
-  socket.on("close", () => console.log("연결끝"));
-  socket.on("message", (message) => {
-    // const translatedMessageData = JSON.parse(message);
-    message = message.toString("utf-8");
-    const msgObj = JSON.parse(message);
-    switch (msgObj.type) {
-      case "new_message":
-        sockets.forEach((soc) => soc.send(`${socket.nickname} : ${msgObj.payload}`));
-        break;
-      case "nickname":
-        socket["nickname"] = msgObj.payload;
-    }
-  });
-});
 
 server.listen(3000, handleListen);
+
+// 여기서의 소켓은 브라우저와의 연결
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anonymous";
+//   console.log(`conneted to Browser 현재 ${sockets.length}`);
+//   socket.on("close", () => console.log("연결끝"));
+//   socket.on("message", (message) => {
+//     // const translatedMessageData = JSON.parse(message);
+//     message = message.toString("utf-8");
+//     const msgObj = JSON.parse(message);
+//     switch (msgObj.type) {
+//       case "new_message":
+//         sockets.forEach((soc) => soc.send(`${socket.nickname} : ${msgObj.payload}`));
+//         break;
+//       case "nickname":
+//         socket["nickname"] = msgObj.payload;
+//     }
+//   });
+// });
 
 // {
 //   type:"message",
